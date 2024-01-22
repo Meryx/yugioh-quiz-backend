3662c5b9d0754b85713e730520a8f99dafb2751d
Meryx <anwar.haredy@gmail.com>
2024-01-22 11:57:40 +0300

    refacotred with list comprehension

--- YGOQuiz/quiz/views.py
+++ YGOQuiz/quiz/views.py
@@ -16,6 +16,19 @@ IMG_EXT = 'jpg'
 IMG_CONTENT_TYPE = "image/jpeg"
 IMG_TYPE = "JPEG"
 
+MONSTER_TYPES = [
+    'Zombie', 'Warrior', 'Spellcaster', 'Fiend', 'Fairy', 'Beast-Warrior', 'Beast',
+    'Winged Beast', 'Aqua', 'Pyro', 'Thunder', 'Dragon', 'Machine', 'Rock', 'Insect',
+    'Cyberse', 'Plant', 'Dinosaur', 'Reptile', 'Fish', 'Sea Serpent', 'Psychic',
+    'Wyrm', 'Divine-Beast', 'Illusion'
+]
+
+MONSTER_ATTRIBUTES = ['DARK', 'EARTH', 'FIRE', 'LIGHT', 'WATER', 'WIND', 'DIVINE']
+
+ALL_MONSTER_DEF = [str(i) for i in range(0, 5000, 50)]
+ALL_MONSTER_ATK = [str(i) for i in range(0, 5000, 50)]
+WEIGHTS = [5 if int(defense) <= 3000 else 1 for defense in ALL_MONSTER_DEF]
+
 def resize_image(image):
     return image.resize(IMG_SIZE, IMG_RESAMPLE_MODE)
 
@@ -92,125 +105,99 @@ def generate_spell_or_trap_question(json_data, images_json, random_image_filenam
 def generate_monster_name_question(json_data, images_json, random_image_filename):
     return generate_name_question(json_data, images_json, random_image_filename)
 
-def generate_monster_atk_question(json_data, images_json, random_image_filename):
-    #random number from 0 to 5000 in multiples of 50
-    all_monster_atk = [str(i) for i in range(0, 5000, 50)]
-    # Defining weights
-    weights = [5 if int(atk) <= 3000 else 1 for atk in all_monster_atk]
-
-    # Picking 3 unique elements
+def generate_monster_atk_question(json_data, *args, **kwargs):
+    correct_attack = str(json_data['atk'])
+    possible_choices = set(ALL_MONSTER_ATK) - {correct_attack}
+    
     selected_atks = set()
     while len(selected_atks) < 3:
-        c = random.choices(all_monster_atk, weights=weights, k=1)[0]
-        if c != str(json_data['atk']):
-            selected_atks.add(str(c))
+        selected_atk = random.choices(list(possible_choices), weights=[WEIGHTS[ALL_MONSTER_ATK.index(atk)] for atk in possible_choices], k=1)[0]
+        selected_atks.add(selected_atk)
+        possible_choices.remove(selected_atk)
 
-    selected_atks = list(selected_atks)
-    random.shuffle(all_monster_atk)
-    correct_choice = str(json_data['atk'])
-    choices = selected_atks
-    choices.append(correct_choice)
+    choices = list(selected_atks) + [correct_attack]
     random.shuffle(choices)
     question = 'What is the ATK of this monster?'
+
     return {
         "question": question,
         "choices": choices,
-        "correct_choice": correct_choice
+        "correct_choice": correct_attack
     }
 
-def generate_monster_def_question(json_data, images_json, random_image_filename):
-    #random number from 0 to 5000 in multiples of 50
-    all_monster_def = [str(i) for i in range(0, 5000, 50)]
-    # Defining weights
-    weights = [5 if int(defense) <= 3000 else 1 for defense in all_monster_def]
-
-    # Picking 3 unique elements
+def generate_monster_def_question(json_data, *args, **kwargs):
+    correct_defense = str(json_data['defense'])
+    possible_choices = set(ALL_MONSTER_DEF) - {correct_defense}
+    
     selected_defs = set()
     while len(selected_defs) < 3:
-        c = random.choices(all_monster_def, weights=weights, k=1)[0]
-        if c != str(json_data['defense']):
-            selected_defs.add(str(c))
+        selected_def = random.choices(list(possible_choices), weights=[WEIGHTS[ALL_MONSTER_DEF.index(defense)] for defense in possible_choices], k=1)[0]
+        selected_defs.add(selected_def)
+        possible_choices.remove(selected_def)
 
-    selected_defs = list(selected_defs)
-    random.shuffle(all_monster_def)
-    correct_choice = str(json_data['defense'])
-    choices = selected_defs
-    choices.append(correct_choice)
+    choices = list(selected_defs) + [correct_defense]
     random.shuffle(choices)
     question = 'What is the DEF of this monster?'
+
     return {
         "question": question,
         "choices": choices,
-        "correct_choice": correct_choice
+        "correct_choice": correct_defense
     }
 
 def generate_monster_level_question(json_data, images_json, random_image_filename):
     if json_data['type'] == 'Link Monster':
         all_monster_levels = ['1', '2', '3', '4', '5', '6']
         correct_choice = str(json_data['linkval'])
-    else:
-        all_monster_levels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        correct_choice = str(json_data['level'])
-    random.shuffle(all_monster_levels)
-    
-    choices = []
-    for choice in all_monster_levels:
-        if choice != correct_choice:
-            choices.append(choice)
-        if choices.__len__() == 3:
-            break
-    choices.append(correct_choice)
-    random.shuffle(choices)
-    question = 'What is the level of this monster?'
-    if json_data['type'] == 'Link Monster':
         question = 'What is the link rating of this monster?'
-    if json_data['type'] == 'XYZ Monster':
+    elif json_data['type'] == 'XYZ Monster':
+        all_monster_levels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+        correct_choice = str(json_data['level'])
         question = 'What is the rank of this monster?'
+    else:
+        all_monster_levels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+        correct_choice = str(json_data['level'])
+        question = 'What is the level of this monster?'
+
+    shuffled_levels = random.sample(all_monster_levels, len(all_monster_levels))
+    incorrect_choices = [lvl for lvl in shuffled_levels if lvl != correct_choice][:3]
+    choices = incorrect_choices + [correct_choice]
+    random.shuffle(choices)
+
     return {
         "question": question,
         "choices": choices,
         "correct_choice": correct_choice
     }
 
-def generate_monster_attribute_question(json_data, images_json, random_image_filename):
-    all_monster_attributes = ['DARK', 'EARTH', 'FIRE', 'LIGHT', 'WATER', 'WIND', 'DIVINE']
-    random.shuffle(all_monster_attributes)
-    correct_choice = json_data['attribute']
-    choices = []
-    for choice in all_monster_attributes:
-        if choice != correct_choice:
-            choices.append(choice)
-        if choices.__len__() == 3:
-            break
-    choices.append(correct_choice)
+def generate_monster_attribute_question(json_data, *args, **kwargs):
+
+    shuffled_attributes = random.sample(MONSTER_ATTRIBUTES, len(MONSTER_ATTRIBUTES))
+    correct_attribute = json_data['attribute']
+    incorrect_choices = [attr for attr in shuffled_attributes if attr != correct_attribute][:3]
+    choices = incorrect_choices + [correct_attribute]
     random.shuffle(choices)
     question = 'What is the attribute of this monster?'
+
     return {
         "question": question,
         "choices": choices,
-        "correct_choice": correct_choice
+        "correct_choice": correct_attribute
     }
 
-def generate_monster_type_question(json_data, images_json, random_image_filename):
-    all_monster_types = ['Zombie', 'Warrior', 'Spellcaster', 'Fiend', 'Fairy', 'Beast-Warrior', 'Beast',
-                    'Winged Beast', 'Aqua', 'Pyro', 'Thunder', 'Dragon', 'Machine', 'Rock', 'Insect',
-                    'Cyberse', 'Plant', 'Dinosaur', 'Reptile', 'Fish', 'Sea Serpent', 'Psychic',
-                    'Wyrm', 'Divine-Beast', 'Illusion']
-    random.shuffle(all_monster_types)
-    correct_choice = json_data['race']
-    choices = []
-    for choice in all_monster_types:
-        if choice != correct_choice:
-            choices.append(choice)
-        if choices.__len__() == 3:
-            break
-    choices.append(correct_choice)
+def generate_monster_type_question(json_data, *args, **kwargs):
+
+    shuffled_types = random.sample(MONSTER_TYPES, len(MONSTER_TYPES))
+    correct_type = json_data['race']
+    incorrect_choices = [t for t in shuffled_types if t != correct_type][:3]
+    choices = incorrect_choices + [correct_type]
     random.shuffle(choices)
     question = 'What is the type of this monster?'
+
     return {
         "question": question,
         "choices": choices,
-        "correct_choice": correct_choice
+        "correct_choice": correct_type
     }
 
 def generate_monster_question(json_data, images_json, random_image_filename):
@@ -243,8 +230,8 @@ def generate_quiz_data(json_data, images_json, random_image_filename):
     return generator(json_data, images_json, random_image_filename)
 
 def random_image_info(request):
-    images_json = [file for file in os.listdir(IMG_DIR) if file.lower().endswith('.json')]
 
+    images_json = [file for file in os.listdir(IMG_DIR) if file.lower().endswith('.json')]
     random_image_json = random.choice(images_json)
     random_image_path = os.path.join(IMG_DIR, random_image_json)
 
